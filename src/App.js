294bfe5bd0532bccc3595593a838476f34565481
franco74m <franco74m@gmail.com>
2022-02-22 16:38:02 -0300

    Importación componente Carrousel
    Importación de Bootstrap y del componente Carrousel.
    Se agrego una nueva seccíon con el componente Carrousel.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "bulma/css/bulma.min.css";
+import 'bootstrap/dist/css/bootstrap.css'
 import "./App.css";
 import Navbar from "./components/Navbar";
 import ProductsList from "./components/productsList";
 import Footer from "./components/Footer";
 import logo from './components/logo.png'
 import BrandDescription from "./components/BrandDescription";
+import Carrousel from "./components/Carrousel"
 
 function App() {
   return (
@@ -12,6 +14,9 @@ function App() {
       <section className="header">
         <Navbar className="navbar" />
         <BrandDescription/>
+      </section>
+      <section>
+        <Carrousel />
       </section>
       <section className="products">
         <ProductsList />
